7ced5acb4e1fc0e9525d2e8f7a3b1c418b79fd0c
Aidyn Meiramkhanuly <aidyn@Air-Aidyn.Home>
2024-09-07 01:29:17 +0500

    init README file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# job-post-platform
-job-post-platform
+# Приложение "JobHunter". Поиск и размещение работы.
+
+# Запуск проекта локально(временное решение) 🔥🔥🔥
+```
+cd frontend -> npm dev
+cd backend -> npm run serve
+change apiEndPoint -> client/config.json = {"apiEndPoint": "http://localhost:8080/api"}
+```
+
+# Стек технологий
+- React, JavaScript
+- TAILWIND
+- NodeJS, Express, MongoDB
--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,9 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import ReactFlow, { addEdge, Background, Controls, MiniMap } from 'react-flow-renderer';
-
-const initialElements = [
-  { id: '1', type: 'input', data: { label: 'Start' }, position: { x: 250, y: 5 } },
-  { id: '2', data: { label: 'Consultation' }, position: { x: 250, y: 100 } },
-  { id: '3', data: { label: 'Contact Method' }, position: { x: 250, y: 200 } },
-  { id: '4', data: { label: 'Phone' }, position: { x: 150, y: 300 } },
-  { id: '5', data: { label: 'Message' }, position: { x: 350, y: 300 } },
-  { id: '6', type: 'output', data: { label: 'End' }, position: { x: 250, y: 400 } },
-  { id: 'e1-2', source: '1', target: '2', animated: true },
-  { id: 'e2-3', source: '2', target: '3' },
-  { id: 'e3-4', source: '3', target: '4' },
-  { id: 'e3-5', source: '3', target: '5' },
-  { id: 'e4-6', source: '4', target: '6' },
-  { id: 'e5-6', source: '5', target: '6' },
-];
-const
+import React from 'react'
 
 const App = () => {
-  const [elements, setElements] = useState(initialElements);
-
-  const onElementsRemove = (elementsToRemove) =>
-    setElements((els) => removeElements(elementsToRemove, els));
-  const onConnect = (params) => setElements((els) => addEdge(params, els));
-
-  const saveFlow = () => {
-    const data = { elements };
-    axios.post('/api/saveFlow', data)
-      .then(response => alert('Flow saved!'))
-      .catch(error => alert('Error saving flow'));
-  };
-
   return (
-    <div style={{ height: 500 }}>
-      <ReactFlow elements={elements} onElementsRemove={onElementsRemove} onConnect={onConnect}>
-        <MiniMap />
-        <Controls />
-        <Background />
-      </ReactFlow>
-      <button onClick={saveFlow}>Сохранить</button>
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
